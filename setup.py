449ffbe556b803c2d03243959e8357dce58c2154
gskckrn928262 <gskckrn928262@users.noreply.github.com>
2026-03-01 23:41:08 +0800

    Add python_requires, console entry point, and __main__ guard

--- setup.py
+++ setup.py
@@ -1,2 +1,13 @@
 from setuptools import setup
-setup(name='triage-test-env', version='1.0.0', py_modules=['app'])
+
+setup(
+    name='triage-test-env',
+    version='1.0.0',
+    py_modules=['app'],
+    python_requires='>=3.12',
+    entry_points={
+        'console_scripts': [
+            'triage-test=app:main',
+        ],
+    },
+)
