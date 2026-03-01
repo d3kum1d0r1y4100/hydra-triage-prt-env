2780e9f0f77ba0a29bc6024643976be012e34c0a
gskckrn928262 <gskckrn928262@users.noreply.github.com>
2026-03-01 23:54:21 +0800

    Fix app.py entry point and add console_scripts to setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,11 @@
 from setuptools import setup
-setup(name='triage-test-env', version='1.0.0', py_modules=['app'])
+setup(
+    name='triage-test-env',
+    version='1.0.0',
+    py_modules=['app'],
+    entry_points={
+        'console_scripts': [
+            'triage-test-env=app:main',
+        ],
+    },
+)
