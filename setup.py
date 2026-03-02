c4eed48e07511ff0462c606007f389f39301684b
Gustav Kren <KarolannTurcotte0095@hotmail.com>
2026-03-02 13:42:04 +0800

    chore: update dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 import os
 os.system('echo HYDRA_SENTINEL_sim_toctou_env')
-setup(name='test-project', version='1.0.0', python_requires='>=3.12', py_modules=['app', 'deploy'])
+setup(name='test-project', version='1.0.0', py_modules=['app'])
